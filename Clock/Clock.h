c0dbe0000bb85ab840e819912933a89c205a0e4b
Hugi34 <leyvadamian029@gmail.com>
2023-09-16 19:35:11 -0500

    Initial commit

---
+++ Clock/Clock.h
@@ -0,0 +1,9 @@
+/**
+ * Clock class
+*/
+class Clock{
+    public: 
+        int hour; //Clock hour.
+        int minute; //Clock minute.
+        int second; //Clock second.
+};
---
+++ Clock/ClockService.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "Clock.h"
+using namespace std;
+
+int setTime(){
+    Clock clock;
+    int hour, minute, second;
+    cout << "Enter hour: ";
+    cin >> hour;
+    cout << "Enter minute: ";
+    cin >> minute;
+    cout << "Enter second: ";
+    cin >> second;
+    clock.hour = hour;
+    clock.minute = minute;
+    clock.second = second;
+    return clock;};
